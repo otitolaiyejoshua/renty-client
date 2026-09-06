7b51f472653abc54154634423f489ae6147e3b0c
Otitolaiye Joshua <otitolaiyejoshua42@gmail.com>
2026-09-06 15:07:53 +0100

    Fix production build warnings

--- src/agent/agent-dashboard.js
+++ src/agent/agent-dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, useLocation, Link } from 'react-router-dom';
+import { Outlet, useLocation} from 'react-router-dom';
 import Header from './Header';
 import Navbar from './Navbar';
 import WelcomeMessage from './WelcomeMessage';
